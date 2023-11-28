adb911b8dd98b61e8fb5682da76b27f1fb5d00f2
Ibrokhim1006 <Ibroxim.2001@mail.ru>
2023-11-28 04:44:15 -0500

    user authen success

--- authen/models.py
+++ authen/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.conf import settings
 
 
 class Gender(models.Model):
@@ -9,11 +8,15 @@ class Gender(models.Model):
     def __str__(self):
         return self.name
 
+
 class CustomUser(AbstractUser):
     """Custom Users Table"""
     email_code = models.IntegerField(default=0, null=True, blank=True)
     birth_date = models.DateField(null=True, blank=True)
-    phone = models.CharField(max_length=250, null=
-    True, blank=True)
-    gender_id = models.ForeignKey(Gender, on_delete=models.CASCADE, null=True, blank=True)
+    phone = models.CharField(
+        max_length=250,
+        null=True, blank=True)
+    gender_id = models.ForeignKey(
+        Gender,
+        on_delete=models.CASCADE, null=True, blank=True)
     avatar = models.ImageField(upload_to="avatars/", null=True, blank=True)
